4e0165f2e1dfb0b6fd4b72385977c3ffcd61b618
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-11-21 13:58:40 +0100

    Added Lesson10

--- src/Lesson10.js
+++ src/Lesson10.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Lesson10 extends React.Component{
     constructor(){
@@ -7,15 +8,17 @@ class Lesson10 extends React.Component{
     }
     update(){
         this.setState({
-            a: this.refs.a.value,
-            b: this.refs.b.value
+            a: this.a.value,
+            b: this.refs.b.value,
+            c: ReactDOM.findDOMNode(this.c).value,
+            d: this.d.refs.input.value
         })
     }
     render(){
         return (
             <div>
                 <input
-                    ref="a"
+                    ref={ node => this.a = node} //callback which returns node
                     type="text"
                     onChange={this.update.bind(this)}
                 />  {this.state.a}
@@ -25,8 +28,29 @@ class Lesson10 extends React.Component{
                     type="text"
                     onChange={this.update.bind(this)}
                 />  {this.state.b}
+                <hr />
+                <Input
+                    ref={ component => this.c = component}
+                    update={this.update.bind(this)}
+                />  {this.state.c}
+                <hr />
+                <Input2
+                    ref={ component => this.d = component}
+                    update={this.update.bind(this)}
+                />  {this.state.d}
             </div>
         )
+    }
+}
+
+class Input extends React.Component {
+    render(){
+        return <input type="text" onChange={this.props.update} /> //we are returning a single node
+    }
+}
+class Input2 extends React.Component {
+    render(){
+        return <div><input ref="input" type="text" onChange={this.props.update} /></div>
     }
 }
 
